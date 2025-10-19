e6aa6c088f8f5ff9eadebf968184fc286ad311ed
Jullyo1122 <jullyoc74@gmail.com>
2025-10-18 22:22:46 -0300

    Feat: autenticação criada

--- back-end/auth.py
+++ back-end/auth.py
@@ -7,17 +7,33 @@ class Login(BaseModel):
     email: str
     senha: str
 
+class Cadastrar(BaseModel):
+    nome: str
+    cpf: str
+    email: str
+    senha: str
+
 db = {
-    1: {"email": "luquinhas99@gmail.com", "senha": "Klark25"},
-    2: {"email": "joaokaik@gmail.com", "senha": "famosinho123"},
-    3: {"email": "agostinho@gmail.com", "senha": "arrascapeta123"}
+    1: {"nome": "Lucas Santos","cpf": "123.456.789-10", "email": "luquinhas99@gmail.com", "senha": "Klark25"},
+    2: {"nome": "João Gustavo de Souza", "cpf": "111.213.141-15", "email": "joaokaik@gmail.com", "senha": "famosinho123"},
+    3: {"nome": "Augusto Soares dos Santos ", "cpf": "161.718.192-20", "email": "agostinho@gmail.com", "senha": "arrascapeta123"}
 }
 
 @app.post('/Login')
-def auth(dados: Login):
-    if dados.email in db and db[dados.email]["senha"] == dados.senha:
-         return {
-            "status": "sucesso",
-            "mensagem": "Login realizado!",
-        }
+def login(dados: Login):
+    for user in db.values():
+        if user["email"] == dados.email and user["senha"] == dados.senha:
+            return {
+                "status": "sucesso",
+                "mensagem": "Login realizado!",
+                }
     return {"status": "erro", "mensagem": "Usuário ou senha incorretos"}
+
+@app.post('/Cadastrar')
+def cadastro(dados: Cadastrar):
+    for user in db.values():
+        if user["cpf"] == dados.cpf:
+            return {"status": "erro", "mensagem": "CPF já cadastrado."}
+        if user["email"] == dados.email:
+            return {"status": "erro", "mensagem": "E-mail já cadastrado."}
+    return {"status": "sucesso", "mensagem": "Cadastro realizado com sucesso!"}
